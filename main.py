3d4e9c9642ddaaf09acffdd8e48f63bd5deb4f33
kur <rhdnfka94@gmail.com>
2018-07-16 18:45:52 +0900

    Add evaluation

--- main.py
+++ main.py
@@ -19,14 +19,18 @@ print(eval_gen)
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',
                                     verbose=1, save_best_only=True)
-history = model.fit_generator(my_gen,steps_per_epoch=30,epochs=5,callbacks=[model_checkpoint])
+history = model.fit_generator(my_gen, steps_per_epoch=30, epochs=1, 
+                              validation_data=eval_gen, validation_steps=3,
+                              callbacks=[model_checkpoint])
 #print(dir(history))
 #print(history)
-#print(history.history)
+print(history.history.keys())
 #print(history.epoch)
 #print(history.model)
 print('    train loss:', history.history['loss'])
 print('train accuracy:', history.history['acc'])
+print('    valid loss:', history.history['val_loss'])
+print('valid accuracy:', history.history['val_acc'])
 
 testGene = testGenerator("data/membrane/test")
 results = model.predict_generator(testGene,30,verbose=1)
