6651ce1f9aef95150f7da54b8fe207b0ed485c1c
kur <rhdnfka94@gmail.com>
2018-07-16 18:13:31 +0900

    Modify to N epochs

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ data_gen_args = dict(rotation_range=0.2,
                     horizontal_flip=True,
                     fill_mode='nearest')
 myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+eval_gen = trainGenerator(2,'data/membrane/eval','image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',
+                                    verbose=1, save_best_only=True)
+history = model.fit_generator(myGene,steps_per_epoch=30,epochs=5,callbacks=[model_checkpoint])
+#print(dir(history))
+#print(history)
+#print(history.history)
+#print(history.epoch)
+#print(history.model)
+print('    train loss:', history.history['loss'])
+print('train accuracy:', history.history['acc'])
 
 testGene = testGenerator("data/membrane/test")
 results = model.predict_generator(testGene,30,verbose=1)
