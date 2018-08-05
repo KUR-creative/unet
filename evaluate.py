2f15e561b28de4a98797fa1d7db76d6f04ac65b4
kur <rhdnfka94@gmail.com>
2018-08-05 16:04:49 +0900

    Extract save&result

--- evaluate.py
+++ evaluate.py
@@ -83,13 +83,30 @@ def evaluate(segnet, inputs, answers):
         iou_arr.append( np.asscalar(np.mean(iou_score)) )
     return iou_arr, result_tuples
 
-#def save_eval_results(result_tuples, result_dir):
+def save_eval_results(result_tuples, result_dir):
+    for idx,(org,ans,pred) in enumerate(result_tuples):
+        cv2.imwrite(os.path.join(result_dir, '%d.png' % idx),
+                    (org * 255).astype(np.uint8))
+        cv2.imwrite(os.path.join(result_dir, '%dans.png' % idx),
+                    (ans * 255).astype(np.uint8))
+        cv2.imwrite(os.path.join(result_dir, '%dpred.png' % idx),
+                    (pred * 255).astype(np.uint8))
 
-dataset_dir = 'data/Benigh_74sep/'
+## set source directories
+#dataset_dir = 'data/Benigh_74sep/'
+dataset_dir = 'data/Malignant_91sep/'
 train_dir = os.path.join(dataset_dir,'train')
 valid_dir = os.path.join(dataset_dir,'valid')
 test_dir = os.path.join(dataset_dir,'test')
 
+#eval_summary = 'data/Benigh_74sep/benigh_eval_summary.yml'
+eval_summary = 'data/Malignant_91sep/eval_summary.yml'
+#eval_summary = 'data/seg_data/eval_summary.yml'
+
+model_path = './malignant.h5'
+#model_path = './seg_data.h5'
+
+## load images
 train_inputs = load_imgs(os.path.join(train_dir,'image'))
 train_answers = load_imgs(os.path.join(train_dir,'label'))
 valid_inputs = load_imgs(os.path.join(valid_dir,'image'))
@@ -97,6 +114,7 @@ valid_answers = load_imgs(os.path.join(valid_dir,'label'))
 test_inputs = load_imgs(os.path.join(test_dir,'image'))
 test_answers = load_imgs(os.path.join(test_dir,'label'))
 
+## ready to save results
 eval_result_dir = os.path.join(dataset_dir,'evaluation')
 eval_train_dir = os.path.join(eval_result_dir,'train')
 eval_valid_dir = os.path.join(eval_result_dir,'valid')
@@ -105,10 +123,6 @@ os.makedirs(eval_train_dir, exist_ok=True)
 os.makedirs(eval_valid_dir, exist_ok=True)
 os.makedirs(eval_test_dir, exist_ok=True)
 
-eval_summary = './data/Benigh_74sep/benigh_eval_summary.yml'
-#eval_summary = 'data/seg_data/eval_summary.yml'
-model_path = './benigh.h5'
-#model_path = './seg_data.h5'
 segnet = model.unet(model_path, (None,None,1)) # img h,w must be x16(multiple of 16)
 
 train_iou_arr, train_result_tuples = evaluate(segnet, train_inputs, train_answers)
@@ -127,6 +141,10 @@ with open(eval_summary,'w') as f:
                            #valid_iou_arr = valid_iou_arr,
                            #test_iou_arr = test_iou_arr)))
 
+save_eval_results(train_result_tuples, eval_train_dir)
+save_eval_results(valid_result_tuples, eval_valid_dir)
+save_eval_results(test_result_tuples, eval_test_dir)
+'''
 for idx,(org,ans,pred) in enumerate(train_result_tuples):
     cv2.imwrite(os.path.join(eval_train_dir,"%d.png"%idx),
                 (org * 255).astype(np.uint8))
@@ -135,18 +153,19 @@ for idx,(org,ans,pred) in enumerate(train_result_tuples):
     cv2.imwrite(os.path.join(eval_train_dir,"%dpred.png"%idx),
                 (pred * 255).astype(np.uint8))
 
-for idx,(org,ans,pred) in enumerate(valid_result_tuples):
-    cv2.imwrite(os.path.join(eval_valid_dir,"%d.png"%idx),
+for idx,(org,ans,pred) in enumerate():
+    cv2.imwrite(os.path.join(,"%d.png"%idx),
                 (org * 255).astype(np.uint8))
     cv2.imwrite(os.path.join(eval_valid_dir,"%dans.png"%idx),
                 (ans * 255).astype(np.uint8))
     cv2.imwrite(os.path.join(eval_valid_dir,"%dpred.png"%idx),
                 (pred * 255).astype(np.uint8))
 
-for idx,(org,ans,pred) in enumerate(test_result_tuples):
-    cv2.imwrite(os.path.join(eval_test_dir,"%d.png"%idx),
+for idx,(org,ans,pred) in enumerate():
+    cv2.imwrite(os.path.join(,"%d.png"%idx),
                 (org * 255).astype(np.uint8))
     cv2.imwrite(os.path.join(eval_test_dir,"%dans.png"%idx),
                 (ans * 255).astype(np.uint8))
     cv2.imwrite(os.path.join(eval_test_dir,"%dpred.png"%idx),
                 (pred * 255).astype(np.uint8))
+'''
