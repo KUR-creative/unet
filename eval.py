0b2cbd5e774d6afec957b73043f85a4737b529be
kur <rhdnfka94@gmail.com>
2018-08-07 21:15:11 +0900

    Refactor: Decompose save_eval_dir to make_eval_dir and 2 func call

--- eval.py
+++ eval.py
@@ -76,29 +76,20 @@ def save_eval_imgs(result_tuples, result_dir):
         cv2.imwrite(os.path.join(result_dir, '%dpred.png' % idx),
                     (pred * 255).astype(np.uint8))
 
-def save_eval_directory(dst_dir,
-                        train_iou_arr, valid_iou_arr, test_iou_arr,
-                        train_result_tuples, valid_result_tuples, test_result_tuples,
-                        eval_summary_name = 'eval_summary.yml', 
+def make_eval_directory(eval_dirpath, eval_summary_name = 'eval_summary.yml',
                         train_dir='train',valid_dir='valid',test_dir='test'):
-    eval_result_dirpath = os.path.join(dataset_dir, dst_dir) # experiment result directory...
-    assert not os.path.exists(eval_result_dirpath), eval_result_dirpath + ' is already exists.' 
-
-    eval_train_dirpath = os.path.join(eval_result_dirpath, train_dir)
-    eval_valid_dirpath = os.path.join(eval_result_dirpath, valid_dir)
-    eval_test_dirpath = os.path.join(eval_result_dirpath, test_dir)
+    ''' TODO: input = dictionary that express directory structure. return paths. '''
+    assert not os.path.exists(eval_dirpath), eval_dirpath + ' is already exists.' 
+    eval_summary_path = os.path.join(eval_dirpath, eval_summary_name)
+    eval_train_dirpath = os.path.join(eval_dirpath, train_dir)
+    eval_valid_dirpath = os.path.join(eval_dirpath, valid_dir)
+    eval_test_dirpath = os.path.join(eval_dirpath, test_dir)
     os.makedirs(eval_train_dirpath, exist_ok=True)
     os.makedirs(eval_valid_dirpath, exist_ok=True)
     os.makedirs(eval_test_dirpath, exist_ok=True)
+    return eval_summary_path, eval_train_dirpath, eval_valid_dirpath, eval_test_dirpath
 
-    eval_summary_path = os.path.join(eval_result_dirpath, eval_summary_name)
-    save_eval_summary(eval_summary_path, train_iou_arr,valid_iou_arr,test_iou_arr)
-    print('Evaluation summary is saved!')
-
-    save_eval_imgs(train_result_tuples, eval_train_dirpath)
-    save_eval_imgs(valid_result_tuples, eval_valid_dirpath)
-    save_eval_imgs(test_result_tuples, eval_test_dirpath)
-    print('Evaluation result images are saved!')
+#def make_evaluation(dataset_dir, model_path
 
 if __name__ == '__main__':
     ## set source directories
@@ -107,10 +98,6 @@ if __name__ == '__main__':
     train_dir = os.path.join(dataset_dir,'train')
     valid_dir = os.path.join(dataset_dir,'valid')
     test_dir = os.path.join(dataset_dir,'test')
-
-    #eval_summary = 'data/Benigh_74sep/benigh_eval_summary.yml'
-    eval_summary = 'data/Malignant_91sep/eval_summary.yml'
-    #eval_summary = 'data/seg_data/eval_summary.yml'
 
     model_path = './malignant.h5'
     #model_path = './seg_data.h5'
@@ -130,6 +117,14 @@ if __name__ == '__main__':
     test_iou_arr, test_result_tuples = evaluate(segnet, test_inputs, test_answers)
     print('Evaluation completed!')
 
-    save_eval_directory('evaluation3', 
-                        train_iou_arr, valid_iou_arr, test_iou_arr,
-                        train_result_tuples, valid_result_tuples, test_result_tuples)
+    ## save evaluation results
+    eval_dirpath = os.path.join(dataset_dir, 'evaluation4')
+    summary_path, train_path, valid_path, test_path = make_eval_directory(eval_dirpath)
+
+    save_eval_summary(summary_path, train_iou_arr, valid_iou_arr, test_iou_arr)
+    print('Evaluation summary is saved!')
+
+    save_eval_imgs(train_result_tuples, train_path)
+    save_eval_imgs(valid_result_tuples, valid_path)
+    save_eval_imgs(test_result_tuples, test_path)
+    print('Evaluation result images are saved!')
