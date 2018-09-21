b6a4254882e709c7843155f6e4e23cd85eb34eee
kur <rhdnfka94@gmail.com>
2018-09-22 06:57:21 +0900

    Add weighted categorical crossentropy

--- model.py
+++ model.py
@@ -7,7 +7,6 @@ from keras.models import *
 from keras.layers import *
 from keras.optimizers import *
 from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras import backend as keras
 
 def jaccard_distance(y_true, y_pred, smooth=100, weight1=1.):
     intersection = K.sum(K.abs(y_true * y_pred), axis=-1)
@@ -28,6 +27,36 @@ def jaccard_distance(y_true, y_pred, smooth=100, weight1=1.):
     jac = (intersection + smooth) / (sum_ - intersection + smooth)
     return (1 - jac) * smooth * weight1 #weighted to label 1
 '''
+
+'''
+A weighted version of categorical_crossentropy for keras (2.0.6). This lets you apply a weight to unbalanced classes.
+@url: https://gist.github.com/wassname/ce364fddfc8a025bfab4348cf5de852d
+@author: wassname
+'''
+from keras import backend as K
+def weighted_categorical_crossentropy(weights):
+    '''
+    A weighted version of keras.objectives.categorical_crossentropy
+    
+    Variables:
+        weights: numpy array of shape (C,) where C is the number of classes
+    
+    Usage:
+        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
+        loss = weighted_categorical_crossentropy(weights)
+        model.compile(loss=loss,optimizer='adam')
+    '''
+    weights = K.variable(weights)        
+    def loss(y_true, y_pred):
+        # scale predictions so that the class probas of each sample sum to 1
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
+        # clip to prevent NaN's and Inf's
+        y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
+        # calc
+        loss = y_true * K.log(y_pred) * weights
+        loss = -K.sum(loss, -1)
+        return loss
+    return loss
 
 # https://www.kaggle.com/aglotero/another-iou-metric  
 # iou = tp / (tp + fp + fn)
@@ -207,6 +236,7 @@ def unet(pretrained_weights = None,input_size = (256,256,1),
         loss = lambda y_true,y_pred:jaccard_distance(y_true,y_pred,100)#,weight_1)
     elif loss == 'wbce': 
         loss = build_weighted_binary_crossentropy(weight_0, weight_1)
+    elif loss == 'wc_bce': 
 
     @as_keras_metric
     def mean_iou(y_true, y_pred):
